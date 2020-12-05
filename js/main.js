a29c8c67356caf572bc4ae7dbbe24f5ab13f5524
CRAZYV3N <benom-crazy@hotmail.com>
2020-12-05 15:17:48 -0600

    Vistas terminadas

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 window.onload = function(){
+  
     
-    /* Menu movil */
+
+    /* Funciones para movil */
     if(screen.width < 769) {
+        /* Menu movil */
         let menuMovil = document.querySelector('#menu-movil');
         let nav = document.querySelector('.nav');
         let indNavMo = 1;
@@ -16,6 +19,50 @@ window.onload = function(){
                 nav.style.display = 'none';
                 indNavMo = 1;
             }
+        });
+
+        /* Slider, Paquetes */
+        new Glider(document.getElementById('glider-single01'), {
+            slidesToShow: 1,
+            dots: '#dots01',
+            draggable: true,
+            arrows: {
+              prev: '#glider-prev01',
+              next: '#glider-next01'
+            },
+            easing: function (x, t, b, c, d) {
+              return c*(t/=d)*t + b;
+            }
+          });
+
+          /* Slider, Templates */
+        new Glider(document.getElementById('glider-single02'), {
+          slidesToShow: 1,
+          dots: '#dots02',
+          draggable: true,
+          rewind: true,
+          arrows: {
+            prev: '#glider-prev02',
+            next: '#glider-next02'
+          },
+          easing: function (x, t, b, c, d) {
+            return c*(t/=d)*t + b;
+          }
+        });
+
+        /* Slider, Proyectos */
+        new Glider(document.getElementById('glider-single03'), {
+          slidesToShow: 1,
+          dots: '#dots03',
+          draggable: true,
+          rewind: true,
+          arrows: {
+            prev: '#glider-prev03',
+            next: '#glider-next03'
+          },
+          easing: function (x, t, b, c, d) {
+            return c*(t/=d)*t + b;
+          }
         });
 
     }
