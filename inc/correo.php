fa56b5af5c39e670225acb38664ecb3e7949d020
CRAZYV3N <benom-crazy@hotmail.com>
2020-12-05 21:09:33 -0600

    Version 1.0, Envía email

--- inc/correo.php
+++ inc/correo.php
@@ -1,30 +1,55 @@
 <?php
 
-if(isset($_POST['enviar'])){
-    if(!empty($_POST['nombre']) && !empty($_POST['telefono']) && !empty($_POST['email']) && !empty($_POST['mensaje']) ) {
-        $nombre = $_POST['nombre'];
-        $empresa = $_POST['empresa'];
-        $telefono = $_POST['telefono'];
-        $email = $_POST['correo'];
-        $mensaje = $_POST['mensaje'];
+$nombre = '';
+$empresa = '';
+$telefono = '';
+$correo = '';
+$mensaje = '';
 
-        $mails = "info@brokenjobs.com.mx;" . $email;
-        $asunto = "Solicitud del sitio BrokenJbos.com.mx";
-        $msg = "Nombre: " . $nombre . "\r\n";
-        $msg .= "Empresa: " . $empresa . "\r\n";
-        $msg .= "Teléfono: " . $telefono . "\r\n";
-        $msg .= "Email: " . $email . "\r\n";
-
-        $header = "From: info@brokenjobs.com.mx" . "\r\n";
-        $header .= "Reply-To:" . $email . "\r\n";
-        $header .= "X-Mailer: PHP/" . phpversion();
-        $mail = mail($mails, $asunto, $msg, $header);
-        if($mail) {
-            header('Location: index.html');
-            echo "Enviado exitosamente";
-        }
-
+if(isset($_POST)){
+    if(isset($_POST['nombre'])){
+        $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
     }
+
+    if(isset($_POST['empresa'])){
+        $empresa = filter_var($_POST['empresa'], FILTER_SANITIZE_STRING);
+    }
+
+    if(isset($_POST['correo'])){
+        $correo = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['correo']);    
+        $correo = filter_var($correo, FILTER_VALIDATE_EMAIL);
+    }
+    
+    $telefono = $_POST['telefono'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $correo . "\r\n";
+
+    $msj = "Nombre: " . $nombre;
+    $msj .= "Empresa: " . $empresa . " \r\n ";
+    $msj .= "Teléfono: " . $telefono . " \r\n ";
+    $msj .= "Correo: " . $correo . " \r\n ";
+    $msj .= "Mensaje: " . $mensaje  . " \r\n ";
+
+    $mail = "info@brokenjobs.com.mx";
+
+    $asunto = "Mensaje de " . $nombre . "Desde sitio Web";
+
+    if(mail($mail, $asunto, $msj, $headers)){
+        echo '<p class="tc"> Gracias, se ha enviado correctamente <a class="btn btn-main" href="https://brokenjobs.com.mx"> Volver a inicio </a> </p>';
+        header("Location: https://brokenjobs.com.mx");
+        die();
+    } else {
+        echo '<p class="tc"> Hubo un error al enviar mensaje, si es posible comunicarse con el administrado </p>';
+        header("Location: contacto.php");
+    }
+
+} else {
+    echo '<p class="tc"> Hubo un erro </p>';
+    header("Location: contacto.php");
 }
 
 ?>
