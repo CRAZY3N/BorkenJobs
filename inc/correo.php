c9f75e0b6c2cb9c16fa33d2e337417b59901a2c9
CRAZYV3N <benom-crazy@hotmail.com>
2020-12-08 18:31:40 -0600

    Mejorando la vista para correo

--- inc/correo.php
+++ inc/correo.php
@@ -28,11 +28,18 @@ if(isset($_POST)){
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: noreplay@brokenjobs.com.mx \r\n';
 
-    $msj = "Nombre: " . $nombre . " \n \r\n ";
-    $msj .= "Empresa: " . $empresa . " \n \r\n ";
-    $msj .= "Teléfono: " . $telefono . " \n \r\n ";
-    $msj .= "Correo: " . $correo . " \n \r\n ";
-    $msj .= "Mensaje: " . $mensaje  . " \n \r\n ";
+    $msj = "<h3>Mensaje desde el sitio web de BrokenJobs</h3>";
+    $msj .= "<h4>Nombre: <p>" . $nombre . "</p> \n \r\n</h4>";
+    $msj .= "<h4>Empresa: <p>" . $empresa . "</p> \n \r\n</h4>";
+    $msj .= "<h4>Teléfono: <p>" . $telefono . "</p> \n \r\n</h4>";
+    $msj .= "<h4>Correo: <p>" . $correo . "</p> \n \r\n</h4>";
+    $msj .= "<h4>Mensaje: <p>" . $mensaje  . "</p> \n \r\n</h4>";
+    $msj .= "<br><br> <p>Gracias por comunicarte con nosotros</p>";
+    $msj .= "<p>Nos comunicaremos lo antes posible</p><br>";
+    $msj .= "<b>Brokenjobs - Diseñando las mejores paginas</b>";
+    $msj .= "<p>Teléfono:  +52 56 2189 0948</p>";
+    $msj .= "<p>info@brokenjobs.com.mx</p>";
+
 
     $mail = "info@brokenjobs.com.mx," . $correo;
 
